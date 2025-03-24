c457437f80359765acd4e8517c8fd058c65e17f4
Kasia Śmieszek <khsmiesz@gmail.com>
2025-03-24 16:24:28 +0100

    Add iteration

--- modul3.py
+++ modul3.py
@@ -1,2 +1,5 @@
 zakupy = {"piekarnia": ["chleb", "bułki", "pączek"],
         "warzywniak": ["marchew", "seler", "rukola"]}
+for sklep, rzeczy in zakupy.items():
+    rzeczy_capitalized=[rzecz.capitalize() for rzecz in rzeczy]
+    print(f"Idę do {sklep.capitalize()} i kupuję tam: {rzeczy_capitalized}")
