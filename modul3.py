0104643f78a4598c608b0246ddac1c88135e7713
Kasia Śmieszek <khsmiesz@gmail.com>
2025-03-24 17:23:32 +0100

    Sum items

--- modul3.py
+++ modul3.py
@@ -3,3 +3,7 @@ zakupy = {"piekarnia": ["chleb", "bułki", "pączek"],
 for sklep, rzeczy in zakupy.items():
     rzeczy_capitalized=[rzecz.capitalize() for rzecz in rzeczy]
     print(f"Idę do {sklep.capitalize()} i kupuję tam: {rzeczy_capitalized}")
+zakupy_piekarnia = zakupy["piekarnia"]
+zakupy_warzywniak = zakupy["warzywniak"]
+suma = len(zakupy_piekarnia)+len(zakupy_warzywniak)
+print(f"W sumie kupuję {suma} produktów")
